bc5d4175f971d3d826caf310daf1ec408b3be602
Ece Helvacıoğlu <ece.helvacioglu@yasar.edu.tr>
2025-07-23 13:59:09 +0300

    Render uyumlu host ve port ayarı eklendi

--- atilim_api.py
+++ atilim_api.py
@@ -36,5 +36,8 @@ def get_atilim():
         return jsonify({"error": str(e)}), 500
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
